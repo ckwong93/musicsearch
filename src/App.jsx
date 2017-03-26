a9b93f1eb338edf2682a3027e97b5164bdd8c7e8
Calvin Kwong <ckwong93@gmail.com>
2017-03-25 23:10:38 -0700

    add artist detail and profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap'
+import Profile from './Profile'
+
 
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      query: ''
+      query: '',
+      artist: null
     }
   }
 
@@ -20,7 +23,11 @@ class App extends Component {
       method: 'GET'
     })
     .then(response => response.json())
-    .then(json => console.log('json',json));
+    .then(json => {
+      const artist = json.artists.items[0];
+      console.log('artist',artist);
+      this.setState({artist})
+    });
   }
   render(){
     return(
@@ -45,8 +52,9 @@ class App extends Component {
           </InputGroup>
         </FormGroup>
         <div className="Profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
+          <Profile
+            artist={this.state.artist}
+          />
         </div>
         <div className="Gallery">
           Gallery
