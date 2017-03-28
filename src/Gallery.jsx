7d998f5ea6a20ee1791b5969bc88277af310956c
Calvin Kwong <ckwong93@gmail.com>
2017-03-27 21:07:38 -0700

    add song preview functionality through gallery component

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Gallery extends Component {
+    playAudio(previewUrl){
+      let audio = new Audio(previewUrl);
+      audio.play()
+    }
     render(){
-      console.log('gallery props', this.props);
+      const { tracks } = this.props;
       return(
-        <div>Gallery</div>
+        <div>
+          {tracks.map((track, k) => {
+            console.log('track',track);
+            const trackImg = track.album.images[0].url
+            return (
+              <div
+                key={k}
+                className="track"
+                onClick={ () => this.playAudio(track.preview_url)}
+              >
+                <img
+                  src={trackImg}
+                  className='track-img'
+                  alt='track'
+                />
+                <p
+                  className="track-text"
+                >{track.name}
+                </p>
+              </div>
+            )
+
+          })}
+        </div>
       )
     }
 }
